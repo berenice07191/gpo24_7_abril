0d94893f2acde7bdb9d4ddef7bac3da3d6590fc4
Lesly Berenice Rivera González <lesly.riverag@uanl.edu.mx>
2025-04-07 15:37:05 -0600

    Ya se guardo :)

--- Demo.py
+++ Demo.py
@@ -1,2 +1,16 @@
-class cuadrado:
-    pass
+class Cuadrado:
+    def __init__(self,lado):
+        self.lado = lado
+        
+    def area(self):
+        return self.lado ** 2
+
+Numlado = Cuadrado(25122023)
+print("Lado del cuadrado:",Numlado.area())
+
+
+
+
+
+     
+    
